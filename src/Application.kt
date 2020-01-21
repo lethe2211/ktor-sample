8b4bb1b73757d94d6e2ae0b9e4cb591570b65ce4
lethe2211 <lethe2211@gmail.com>
2020-01-21 16:22:43 +0900

    Add simple requests

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,48 @@
 package com.example
 
+import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.application.*
+import io.ktor.features.ContentNegotiation
+import io.ktor.jackson.jackson
+import io.ktor.request.receive
 import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.routing.routing
+import java.util.Collections
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+data class Snippet(val text: String)
+
+val snippets = Collections.synchronizedList(mutableListOf(
+    Snippet("hello"),
+    Snippet("world")
+))
+
+data class PostSnippet(val snippet: PostSnippet.Text) {
+    data class Text(val text: String)
+}
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
+    routing {
+        get("/snippets") {
+            call.respond(mapOf("snippets" to synchronized(snippets) { snippets.toList() }))
+        }
+        post("/snippets") {
+            val post = call.receive<PostSnippet>()
+            snippets += Snippet(post.snippet.text)
+            call.respond(mapOf("OK" to true))
+        }
+    }
+
 }
 
